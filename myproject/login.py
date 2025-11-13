8370b42d24389e607f8d6071cf178291cf4b7e3d
Hatsu <guilhermelundgren@gmail.com>
2025-11-13 17:00:51 -0300

    first commit

---
+++ myproject/login.py
@@ -0,0 +1,23 @@
+from flask import *
+
+# Usuário e senha "fake" só pra exemplo
+USER = "admin"
+PASSWORD = "1234"
+
+def login_funcao():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        senha = request.form['senha']
+
+        if usuario == USER and senha == PASSWORD:
+            # Redireciona para a página inicial após login bem-sucedido
+            return redirect(url_for('home'))
+        else:
+            # Retorna a página de login com mensagem de erro
+            return render_template('login.html', erro="Usuário ou senha incorretos!")
+
+    # Se for GET, só mostra o formulário
+    return render_template('login.html')  
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ myproject/main.py
@@ -0,0 +1,27 @@
+from flask import *
+from login import login_funcao
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return redirect(url_for('login'))
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    login_funcao()
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/recursos")
+def recursos():
+    return "<p>Pagina de recursos</p>"
+
+@app.route("/funcionarios")
+def funcionarios():
+    return "<p>Pagina de funcionarios</p>"
